2449844741d97ca88b0fd8a2d8aa38893a10b301
drmarx <drmarx@ceas-942-mac13.cae.uwm.edu>
2017-03-07 12:00:37 -0600

    fixed declarations after structure change

--- src/main/java/llm/lab7/server/MainDirectory.java
+++ src/main/java/llm/lab7/server/MainDirectory.java
@@ -1,6 +1,8 @@
-package llm.lab7;
+package llm.lab7.server;
 
 import java.util.*;
+import llm.lab7.Directory;
+import llm.lab7.Employee;
 
 public class MainDirectory implements Directory{
 	private ArrayList<Employee> list = new ArrayList<Employee>();
