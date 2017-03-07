2362a100df81ac5644ef1ad9073c0ea0f12c6a61
Jack Klika <jhklika@uwm.edu>
2017-03-07 12:12:31 -0600

    Added user input in App.java

--- src/main/java/llm/lab7/App.java
+++ src/main/java/llm/lab7/App.java
@@ -1,13 +1,44 @@
 package llm.lab7;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	
+    	boolean powerOn = true;
+    	Scanner scan = new Scanner(System.in);
+    	String cmd;
+    	
+    	while(powerOn){
+    		System.out.print("CMD> ");
+    		cmd = scan.next().toUpperCase();
+    		// process command
+    		
+    		if (cmd.equals("POWER")){
+    			powerOn = false;
+    		}
+    		else if (cmd.equals("ADD")){
+    			// jump into another loop, break with 'end'
+    		
+    			
+    			
+    		}
+    		else if (cmd.equals("CLR")){
+    			// Clear?
+    		}
+    		else if (cmd.equals("PRINT")){
+    			// Print
+    		}
+    		else {
+    			System.out.println("[INVALID COMMAND] Type POWER to quit.");
+    		}
+    	}
+    	
+    	System.out.println("Goodbye!");
     }
 }
