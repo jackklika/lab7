95626deb163d20adf9d1fa42232d8f6d8709e16b
seanjn57 <aldalali@uwm.edu>
2017-03-07 11:53:29 -0600

    Update Employee.java

--- src/main/java/llm/lab7/Employee.java
+++ src/main/java/llm/lab7/Employee.java
@@ -1,5 +1,23 @@
 package llm.lab7;
 
 public class Employee {
+String FirstName;
+String LastName;
+Integer Phone;
+String Department;
+
+	private Employee(String FirstName, String LastName, Integer Phone, String Department){
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.Phone = Phone;
+		this.Department = Department;
+	}
+	
+public String toString(){
+		
+		String s = Phone.toString() ;
+		
+				return s;
+}
 
 }
