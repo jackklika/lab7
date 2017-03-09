38629679e0ca1fe24a23f096a7444052d9b3bfce
Jack Klika <jhklika@uwm.edu>
2017-03-09 11:19:48 -0600

    added some gson to directoryproxy

--- src/main/java/llm/lab7/client/DirectoryProxy.java
+++ src/main/java/llm/lab7/client/DirectoryProxy.java
@@ -3,17 +3,26 @@ package llm.lab7.client;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import com.google.gson.Gson;
+
 import llm.lab7.Directory;
 import llm.lab7.Employee;
+import llm.lab7.server.DirectoryServer;
 
 public class DirectoryProxy implements Directory{
 private ArrayList<Employee> list = new ArrayList<Employee>();
+
 
 	public void add(Collection<Employee> e) {
 		// TODO Auto-generated method stub
 		if(!list.contains(e)){
 			list.addAll(e);
 		}
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(list);
+		System.out.println(json);
+		Transport.send(json);
 	}
 	
 
@@ -32,7 +41,13 @@ private ArrayList<Employee> list = new ArrayList<Employee>();
 	
 	
 	// Sends data to server directory
-	private class Transport {
+	private static class Transport {
+		
+	DirectoryServer ds = new DirectoryServer();
+		
+		public static void send(String json){
+			
+		}
 		
 	}
 
