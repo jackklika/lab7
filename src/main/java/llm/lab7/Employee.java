6779401cdf93065b6875f95d6efed0c9b03834c6
Jack Harvey Klika <jhklika@ceas-942-mac14.cae.uwm.edu>
2017-03-14 12:18:08 -0500

    Changes to Employee

--- src/main/java/llm/lab7/Employee.java
+++ src/main/java/llm/lab7/Employee.java
@@ -1,35 +1,31 @@
 package llm.lab7;
 
-import java.util.Comparator;
 
-public class Employee {
+public class Employee implements Comparable<Object>{
+	
 	public String firstName;
 	public String lastName;
 	public String department;
-	public String phone;
-
-	Employee(String firstName, String lastName, String department, String phone) {
+	public String phoneNumber;
+	
+	public Employee(String firstName, String lastName, String department, String phoneNum) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.department = department;
-		this.phone = phone;
+		this.phoneNumber = phoneNum;
 	}
-
+	
+	@Override
 	public String toString() {
-
-		String s = phone.toString();
-		return s;
+		return firstName + " " + lastName + " \tin " + department + " \thas phone number of " + phoneNumber;
 	}
- 
-	//Comparator to sort list items by last name
-	//based on example here http://beginnersbook.com/2013/12/java-arraylist-of-object-sort-example-comparable-and-comparator/
-	public static Comparator<Employee> NameComparator = new Comparator<Employee>(){
-		public int compare(Employee e1, Employee e2){
-			String name1 = e1.lastName;
-			String name2 = e2.lastName;
-			
-			return name1.compareTo(name2);
+
+	public int compareTo(Object o) {
+		if (o instanceof Employee) {
+			Employee other = (Employee) o;
+			return lastName.compareTo(other.lastName);
 		}
-	};
+		return 0;
+	}
 
 }
